21b6a21e5f4234d27d0938ecbeeeb536f7abc6f0
Fathaah <abdulfta12@gmail.com>
2020-02-26 20:11:11 +0530

    Lowe

---
+++ ReadMe.txt
@@ -0,0 +1,15 @@
+Completed:
+
+A star algorithm implemented to find a path from a given map image.
+
+Input: Image(Map of the store )
+
+Output: Set of path to the destination product
+
+To Do:
+
+Implement a database of products and its location (Shelf number, which side of the shelf and position in the shelf)
+
+Improve UI
+
+Implement the above prototype in React Native
---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import sys
+import cv2
+import matplotlib.pyplot as plt
+import astar
+np.set_printoptions(threshold=sys.maxsize)
+img = cv2.imread('m2.png')
+img = cv2.resize(img, (64,64))
+im = np.asarray(img)
+#print(im)
+im = np.asarray((im[:,:,1] < 200 ).astype(np.uint))
+plt.imshow(im, cmap = 'gray')
+plt.show()
+#print((im).astype(np.uint))
+path = astar.find_path(im,(55,30),(2,3))
+print(path[0][1])
+for i in range(len(path)):
+    img[path[i][0],path[i][1]] = 127
+
+plt.imshow(img) 
+plt.show()
+
+
+
